a841dcbc2a213626eb156852e14eb9a7153b18cd
David Abram-Krismer <david.krismer71@gmail.com>
2023-03-07 21:18:19 -0800

    Initial Commit

---
+++ ICS224_MidtermJumpstart/ICS224_MidtermJumpstartApp.swift
@@ -0,0 +1,17 @@
+//
+//  ICS224_MidtermJumpstartApp.swift
+//  ICS224_MidtermJumpstart
+//
+//  Created by ICS 224 on 2023-03-07.
+//
+
+import SwiftUI
+
+@main
+struct ICS224_MidtermJumpstartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
